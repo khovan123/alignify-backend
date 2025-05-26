e2ea4883c0de12673d8880cfa0f2979c32e086be
Phan Nguyễn Quốc Minh <152273723+khovan123@users.noreply.github.com>
2025-05-26 23:30:31 +0700

    <feat/util>: add decode token, create token, hashPassword, create URL reset password, check password and check is owner --function (#8)

---
+++ src/main/java/com/api/util/Helper.java
@@ -0,0 +1,14 @@
+package com.api.util;
+
+import com.auth0.jwt.interfaces.DecodedJWT;
+import jakarta.servlet.http.HttpServletRequest;
+
+public class Helper {
+
+    public static boolean isOwner(String id, HttpServletRequest request) {
+        DecodedJWT decodeJWT = JwtUtil.decodeToken(request);
+        String ownerId = decodeJWT.getSubject();
+        return ownerId.equalsIgnoreCase(id);
+    }
+
+}
