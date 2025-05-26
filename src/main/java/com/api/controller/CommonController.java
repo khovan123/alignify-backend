61cc971b4bfc8935633e46300524b90dfcd6920f
khovan <minhpnq1807@gmail.com>
2025-05-26 23:35:51 +0700

    <feat/api>: add Role endpoint for user mangement && add Category endpoint for content classification

---
+++ src/main/java/com/api/controller/CommonController.java
@@ -0,0 +1,34 @@
+package com.api.controller;
+
+import com.api.repository.*;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class CommonController {
+
+    @Autowired
+    private RoleRepository roleRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @GetMapping("/role")
+    public ResponseEntity<?> getAllRole() {
+        return ResponseEntity.status(200).body(Map.of(
+                "data", Map.of("role", roleRepository.findAll())
+        ));
+    }
+    
+    @GetMapping("/category")
+    public ResponseEntity<?> getAllCategory() {
+        return ResponseEntity.status(200).body(Map.of(
+                "data", Map.of("category", categoryRepository.findAll())
+        ));
+    }
+
+}
