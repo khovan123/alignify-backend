5f7ef5c4ef9e4a07bcdbd8a653f2bef507257f98
Phan Nguyễn Quốc Minh <152273723+khovan123@users.noreply.github.com>
2025-05-29 23:39:32 +0700

    Revert "<feat/avatar>: change avatar, <chore/dockerfile>: add dockerfile (#21)" (#22)
    This reverts commit f965d527761206346b7ba2ccbd9431252275e596.

--- src/main/java/com/api/config/EnvConfig.java
+++ src/main/java/com/api/config/EnvConfig.java
@@ -1,24 +1,11 @@
 package com.api.config;
 
 import com.auth0.jwt.algorithms.Algorithm;
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 public class EnvConfig {
 
-    @Value("${spring.api.secret-key}")
-    private String secretKey;
-
-    public static Algorithm ALGORITHM;
-
+    public static final Algorithm ALGORITHM = Algorithm.HMAC256("secretKey");
     public static String BRAND_ROLE_ID = "6834312517f64328628a78dc";
     public static String INFLUENCER_ROLE_ID = "6834312517f64328628a78dd";
     public static String ADMIN_ROLE_ID = "6834312517f64328628a78db";
-
-    @PostConstruct
-    public void init() {
-        ALGORITHM = Algorithm.HMAC256(secretKey);
-    }
 }
