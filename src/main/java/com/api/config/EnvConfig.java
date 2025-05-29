f965d527761206346b7ba2ccbd9431252275e596
Phan Nguyễn Quốc Minh <152273723+khovan123@users.noreply.github.com>
2025-05-29 23:38:11 +0700

    <feat/avatar>: change avatar, <chore/dockerfile>: add dockerfile (#21)

--- src/main/java/com/api/config/EnvConfig.java
+++ src/main/java/com/api/config/EnvConfig.java
@@ -1,11 +1,24 @@
 package com.api.config;
 
 import com.auth0.jwt.algorithms.Algorithm;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class EnvConfig {
 
-    public static final Algorithm ALGORITHM = Algorithm.HMAC256("secretKey");
+    @Value("${spring.api.secret-key}")
+    private String secretKey;
+
+    public static Algorithm ALGORITHM;
+
     public static String BRAND_ROLE_ID = "6834312517f64328628a78dc";
     public static String INFLUENCER_ROLE_ID = "6834312517f64328628a78dd";
     public static String ADMIN_ROLE_ID = "6834312517f64328628a78db";
+
+    @PostConstruct
+    public void init() {
+        ALGORITHM = Algorithm.HMAC256(secretKey);
+    }
 }
