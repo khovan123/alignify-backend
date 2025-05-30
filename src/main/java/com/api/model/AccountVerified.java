65006a26a82da1c8e4defdc7f7aa00aa6b3af547
Phan Nguyễn Quốc Minh <152273723+khovan123@users.noreply.github.com>
2025-05-30 23:38:26 +0700

    <feat/auth>: block register without verify email (#37)
    * <feat/auth>: add feature verify email with OTP and security OTP in authentication
    
    * <fix/auth-otp>: add create collection for otp
    
    * <feat>: block register without verify email
    
    ---------
    
    Co-authored-by: nhibao08 <lebaonhi0805@gmail.com>

---
+++ src/main/java/com/api/model/AccountVerified.java
@@ -0,0 +1,54 @@
+package com.api.model;
+
+import java.time.LocalDate;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "otpVerifications")
+public class AccountVerified {
+
+    @Id
+    private String verification_id;
+    private String email;
+    @CreatedDate
+    private LocalDate createdAt;
+
+    public AccountVerified() {
+    }
+
+    public AccountVerified(String email) {
+        this.email = email;
+    }
+
+    public AccountVerified(String verification_id, String email, LocalDate createdAt) {
+        this.verification_id = verification_id;
+        this.email = email;
+        this.createdAt = createdAt;
+    }
+
+    public String getVerification_id() {
+        return verification_id;
+    }
+
+    public void setVerification_id(String verification_id) {
+        this.verification_id = verification_id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+}
