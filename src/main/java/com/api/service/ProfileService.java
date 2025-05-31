8ce21dcdab65c24ed6451c306b3c74f506aa0256
khovan <minhpnq1807@gmail.com>
2025-05-31 14:01:08 +0700

    <chore>: config response

--- src/main/java/com/api/service/ProfileService.java
+++ src/main/java/com/api/service/ProfileService.java
@@ -15,7 +15,6 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
