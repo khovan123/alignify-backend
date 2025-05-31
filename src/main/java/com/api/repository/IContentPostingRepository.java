f6de1344a9770736945d77abe7c1de7bbdf9c5e4
Kangnvd <ngocthiennguyen28052004@gmail.com>
2025-05-31 16:36:33 +0700

    <feat/post>: add CRUD for content Posting

---
+++ src/main/java/com/api/repository/IContentPostingRepository.java
@@ -0,0 +1,12 @@
+
+package com.api.repository;
+
+import com.api.model.ContentPosting;
+import java.util.List;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+public interface IContentPostingRepository extends MongoRepository<ContentPosting, String>{
+    List<ContentPosting> findByInfluencerID(String influencerID);
+
+}
