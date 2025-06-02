5bcd4a58b56486982a02b81f7d597118637c34a5
khovan <minhpnq1807@gmail.com>
2025-06-02 23:07:09 +0700

    <fix/categoryRepo>: change query

---
+++ src/main/java/com/api/controller/CommonController.java
@@ -0,0 +1,31 @@
+package com.api.controller;
+
+import com.api.dto.ApiResponse;
+import com.api.repository.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class CommonController {
+
+    @Autowired
+    private RoleRepository roleRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @GetMapping("/role")
+    public ResponseEntity<?> getAllRole(HttpServletRequest request) {
+        return ApiResponse.sendSuccess(200, "Roles", roleRepository.findAll(), request.getRequestURI());
+    }
+
+    @GetMapping("/category")
+    public ResponseEntity<?> getAllCategory(HttpServletRequest request) {
+        return ApiResponse.sendSuccess(200, "Categories", categoryRepository.findAll(), request.getRequestURI());
+    }
+
+}
