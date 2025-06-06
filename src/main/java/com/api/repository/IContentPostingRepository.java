e423ac9fc187f517fb9b9776842948d58440fb1f
Kangnvd <ngocthiennguyen28052004@gmail.com>
2025-06-06 13:42:45 +0700

    <docs>: fix bug

--- src/main/java/com/api/repository/IContentPostingRepository.java
+++ src/main/java/com/api/repository/IContentPostingRepository.java
@@ -3,13 +3,14 @@ package com.api.repository;
 
 import com.api.model.ContentPosting;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IContentPostingRepository extends MongoRepository<ContentPosting, String>{
     List<ContentPosting> findByUserId(String userId);
-    List<ContentPosting> findByIsPublicTrue();
+    List<ContentPosting> findByIsPublicTrue(Pageable pageable);
      
 
 }
