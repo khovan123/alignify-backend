cce43ec442df26b8f9fe4aa819d112ebddda3f06
Phan Nguyễn Quốc Minh <152273723+khovan123@users.noreply.github.com>
2025-06-08 00:56:08 +0700

    <feat/mapper>: add object mapper for request in RequestBody (#108)
    * <config/mongo>: change type in influencer model
    
    * <feat/security>: using AuthenticationPrincipal
    
    * <feat/mapper>: add object mapper for request in RequestBody

---
+++ src/main/java/com/api/dto/InfluencerProfileRequest.java
@@ -0,0 +1,66 @@
+package com.api.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+public class InfluencerProfileRequest {
+
+    private LocalDateTime DoB;
+    private String gender;
+    private String bio;
+    private Map<String, String> socialMediaLinks;
+    private List<String> categoryIds;
+
+    public InfluencerProfileRequest( LocalDateTime DoB, String gender, String bio, Map<String, String> socialMediaLinks, List<String> categoryIds) {
+        this.DoB = DoB;
+        this.gender = gender;
+        this.bio = bio;
+        this.socialMediaLinks = socialMediaLinks;
+        this.categoryIds = categoryIds;
+    }
+
+    public InfluencerProfileRequest() {
+    }
+
+    public LocalDateTime getDoB() {
+        return DoB;
+    }
+
+    public void setDoB(LocalDateTime DoB) {
+        this.DoB = DoB;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public Map<String, String> getSocialMediaLinks() {
+        return socialMediaLinks;
+    }
+
+    public void setSocialMediaLinks(Map<String, String> socialMediaLinks) {
+        this.socialMediaLinks = socialMediaLinks;
+    }
+
+    public List<String> getCategoryIds() {
+        return categoryIds;
+    }
+
+    public void setCategoryIds(List<String> categoryIds) {
+        this.categoryIds = categoryIds;
+    }
+
+}
--- src/main/java/com/api/dto/RecoveryPasswordRequest.java
+++ src/main/java/com/api/dto/RecoveryPasswordRequest.java
@@ -1,11 +1,15 @@
 package com.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class RecoveryPasswordRequest {
 
+    @JsonProperty("email")
     private String email;
+    @JsonProperty("url")
     private String url;
 
-    public RecoveryPasswordRequest(String email, String url) {
+    public RecoveryPasswordRequest(@JsonProperty("email") String email, @JsonProperty("url") String url) {
         this.email = email;
         this.url = url;
     }
