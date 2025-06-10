e7dda6979d70bf575ab6a33def34aa03f5f2c2c6
Nguyễn Ngọc Thiện <de180945nguyenngocthien@gmail.com>
2025-06-10 12:23:02 +0700

    <feat/contentPosting-comment>: create CRUD and like for comment (#99)
    * <feat/post>: add CRUD for content Posting
    
    * <feat/contentPosting>: fix model and contentPosting service
    
    * <feat/CRUD>: fix and update CRUD and add attribute timeStamp for contentPosting
    
    * merge between master and feat/contentPosting
    
    * <docs>: update .gitignore and up to date (#75)
    
    * chore(config): configure schema database and constant value
    
    * <docs>: update .gitignore and up to date
    
    * <fix>: add AuthService.java again after filter-repo (#76)
    
    * <feat/method-http>: change method http and delete Mail entity class
    
    * <fix>: add AuthService again after filter-repo
    
    * <fix/categoryRepo>: change query (#77)
    
    * <feat/api>: add Role endpoint for user mangement && add Category endpoint for content classification
    
    * <fix/categoryRepo>: change query
    
    * <chore/deployed-file>: delete target/maven-status/maven-compiler-plugin directory
    
    * <chore/delete-deployed file>: delete target/maven-archiver directory
    
    * <fix>: match function name in cate-repo (#79)
    
    * <feat/method-http>: change method in profile
    
    * <feat/fix-profile>: fix query, change method http, add delete image, user
    
    * <fix>: match function name in cate-repo
    
    * <fix>: match function name in cate-repo
    
    * <fix>: match function name in cate-repo
    
    * <fix>: match function name in cate-repo
    
    * <deployed>:  add jar file (#80)
    
    * <feat/ContentPosting>: fix and update CRUD for contentPosting
    
    * <feat/ContentPosting>: Update CRUD for contentPosting
    
    * <feat/ContentPosting>: Fix function toggleLike
    
    * fix conflict and fix response code
    
    * <fix/campaign>: fix response code
    
    * <feat/contentPosting>: fix require from comment pull request
    
    * <docs>: update gitignore
    
    * <docs>: update gitignore
    
    * feat response from comment pull request
    
    * <feat/comment>: create CRUD for comment and like for comment
    
    * delete attribute like for comment
    
    * <fix/comment>: transfer commentIDs type List become commentCount type int
    
    * <fix/comment>: completed comment after fix param for function suitable with requirement
    
    ---------
    
    Co-authored-by: Kangnvd <ngocthiennguyen28052004@gmail.com>
    Co-authored-by: Phan Nguyễn Quốc Minh <152273723+khovan123@users.noreply.github.com>

---
+++ src/main/java/com/api/model/ContentPosting.java
@@ -0,0 +1,116 @@
+package com.api.model;
+
+import java.util.Date;
+import java.util.List;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "contentPostings")
+public class ContentPosting {
+
+    @Id
+    private String contentId;
+
+    private String userId;
+    private String content;
+    private String imageUrl;
+    private List<String> categoryIds;
+
+    @CreatedDate
+    private Date timestamp;
+    private boolean isPublic;
+    private int commentCount;
+    private int like;
+
+    public ContentPosting() {
+        this.like=0;
+        this.commentCount = 0;
+        this.isPublic=true;
+    }
+
+    public ContentPosting(String contentId, String userId, String content, String imageUrl, List<String> categoryIds, Date timestamp, boolean isPublic, int commentCount, int like) {
+        this.contentId = contentId;
+        this.userId = userId;
+        this.content = content;
+        this.imageUrl = imageUrl;
+        this.categoryIds = categoryIds;
+        this.timestamp = timestamp;
+        this.isPublic = isPublic;
+        this.commentCount = commentCount;
+        this.like = like;
+    }
+
+    public String getContentId() {
+        return contentId;
+    }
+
+    public void setContentId(String contentId) {
+        this.contentId = contentId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public List<String> getCategoryIds() {
+        return categoryIds;
+    }
+
+    public void setCategoryIds(List<String> categoryIds) {
+        this.categoryIds = categoryIds;
+    }
+
+    public boolean isIsPublic() {
+        return isPublic;
+    }
+
+    public void setIsPublic(boolean isPublic) {
+        this.isPublic = isPublic;
+    }
+
+    public int getCommentCount() {
+        return commentCount;
+    }
+
+    public void setCommentCount(int commentCount) {
+        this.commentCount = commentCount;
+    }
+
+    public int getLike() {
+        return like;
+    }
+
+    public void setLike(int like) {
+        this.like = like;
+    }
+
+}
