3cf96b496273bf565835d06bd0c05a7743d38924
Kangnvd <ngocthiennguyen28052004@gmail.com>
2025-06-24 13:49:58 +0700

    <fix>: commit to pull main

--- src/main/java/com/api/repository/CampaignRepository.java
+++ src/main/java/com/api/repository/CampaignRepository.java
@@ -30,5 +30,6 @@ public interface CampaignRepository extends MongoRepository<Campaign, String> {
     List<Campaign> findAllByCampaignIdIn(Set<String> campaignIds);
 
     Page<Campaign> findByCategoryIdsInAndStatusOrderByCreatedAtDesc(String category, String status, Pageable pageable);
-
+    
+    List<Campaign> findAllByOrderByApplicationTotalDesc();
 }
