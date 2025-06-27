b217b78c28a0ce39a948ea7a2ce1a11ec35a2b84
khovan <minhpnq1807@gmail.com>
2025-06-27 23:56:50 +0700

    <fix/likes>: add annotation @controller

--- src/main/java/com/api/controller/websocket/likes/LikesController.java
+++ src/main/java/com/api/controller/websocket/likes/LikesController.java
@@ -9,11 +9,13 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
 
 import com.api.model.Likes;
 import com.api.repository.LikesRepository;
 import com.api.security.StompPrincipal;
 
+@Controller
 public class LikesController {
   @Autowired
   private LikesRepository likesRepository;
