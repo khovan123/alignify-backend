511db3da34fff896ca59921ccf89c2f3a367fae8
khovan <minhpnq1807@gmail.com>
2025-06-27 22:52:00 +0700

    <feat/likes>: create websocket for like contentPosting

---
+++ src/main/java/com/api/controller/websocket/likes/LikesController.java
@@ -0,0 +1,41 @@
+package com.api.controller.websocket.likes;
+
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+
+import com.api.model.Likes;
+import com.api.repository.LikesRepository;
+import com.api.security.StompPrincipal;
+
+public class LikesController {
+  @Autowired
+  private LikesRepository likesRepository;
+  @Autowired
+  private SimpMessagingTemplate messagingTemplate;
+
+  @MessageMapping("/like/{contentId}")
+  public void toggleLike(
+      @Payload Likes likes,
+      @DestinationVariable("contentId") String contentId,
+      Principal principal) {
+    if (principal == null || principal.getName() == null) {
+      throw new SecurityException("Access is denied for: " + contentId);
+    }
+    if (principal instanceof StompPrincipal stompPrincipal) {
+      if (likesRepository.existsByContentIdAndUserId(contentId, stompPrincipal.getUserId())) {
+        likesRepository.deleteByUserIdAndContentId(stompPrincipal.getUserId(), contentId);
+      } else {
+        likesRepository.save(likes);
+      }
+      messagingTemplate.convertAndSend("/topic/contents/" + contentId, likes);
+    } else {
+      throw new SecurityException("Invalid principal type");
+    }
+  }
+
+}
