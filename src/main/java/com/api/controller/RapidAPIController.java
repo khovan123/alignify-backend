7738a1c8589db04615f0b4a3f2ce54188d08e23f
khovan <minhpnq1807@gmail.com>
2025-07-01 18:17:29 +0700

    <fix/slice>: fix dup mapping endpoint

--- src/main/java/com/api/controller/RapidAPIController.java
+++ src/main/java/com/api/controller/RapidAPIController.java
@@ -1,14 +1,15 @@
 package com.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.api.service.RapidAPIService;
+
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequestMapping("/api/v1/rapidapi")
@@ -17,23 +18,27 @@ public class RapidAPIController {
     @Autowired
     private RapidAPIService rapidAPIService;
 
-    @GetMapping("/tiktok/{uniqueId}/statsV2")
-    public ResponseEntity<?> getUserStatsFromTiktok(@PathVariable("uniqueId") String uniqueId, HttpServletRequest request) {
+    @GetMapping("/tiktok/user/{uniqueId}/statsV2")
+    public ResponseEntity<?> getUserStatsFromTiktok(@PathVariable("uniqueId") String uniqueId,
+            HttpServletRequest request) {
         return rapidAPIService.getUserStatsFromTiktok(uniqueId, request);
     }
-    
-    @GetMapping("/tiktok/{videoId}/statsV2")
-    public ResponseEntity<?> getVideoStatsFromTiktok(@PathVariable("videoId") String videoId, HttpServletRequest request){
+
+    @GetMapping("/tiktok/video/{videoId}/statsV2")
+    public ResponseEntity<?> getVideoStatsFromTiktok(@PathVariable("videoId") String videoId,
+            HttpServletRequest request) {
         return rapidAPIService.getVideoDetailsFromTiktok(videoId, request);
     }
-    
+
     @GetMapping("/youtube/{channelName}/subscriber_count")
-    public ResponseEntity<?> getSubcriberCountChannelFromYoutube(@PathVariable("channelName")String channelName, HttpServletRequest request){
+    public ResponseEntity<?> getSubcriberCountChannelFromYoutube(@PathVariable("channelName") String channelName,
+            HttpServletRequest request) {
         return rapidAPIService.getStatsChannelFromYoutube(channelName, request);
     }
-    
+
     @GetMapping("/youtube/{videoId}")
-    public ResponseEntity<?> getVideoDetatilsFromYoutube(@PathVariable("videoID")String videoId, HttpServletRequest request){
+    public ResponseEntity<?> getVideoDetatilsFromYoutube(@PathVariable("videoID") String videoId,
+            HttpServletRequest request) {
         return rapidAPIService.getVideoDetailsFromYoutube(videoId, request);
     }
 }
