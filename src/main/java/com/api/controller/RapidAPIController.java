97e126e7c099e2ce6e84efeab110857db6857f5b
khovan <minhpnq1807@gmail.com>
2025-07-01 17:59:39 +0700

    <feat/socialStats>: supported for youtube & tiktok

---
+++ src/main/java/com/api/controller/RapidAPIController.java
@@ -0,0 +1,39 @@
+package com.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.api.service.RapidAPIService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+@RequestMapping("/api/v1/rapidapi")
+public class RapidAPIController {
+
+    @Autowired
+    private RapidAPIService rapidAPIService;
+
+    @GetMapping("/tiktok/{uniqueId}/statsV2")
+    public ResponseEntity<?> getUserStatsFromTiktok(@PathVariable("uniqueId") String uniqueId, HttpServletRequest request) {
+        return rapidAPIService.getUserStatsFromTiktok(uniqueId, request);
+    }
+    
+    @GetMapping("/tiktok/{videoId}/statsV2")
+    public ResponseEntity<?> getVideoStatsFromTiktok(@PathVariable("videoId") String videoId, HttpServletRequest request){
+        return rapidAPIService.getVideoDetailsFromTiktok(videoId, request);
+    }
+    
+    @GetMapping("/youtube/{channelName}/subscriber_count")
+    public ResponseEntity<?> getSubcriberCountChannelFromYoutube(@PathVariable("channelName")String channelName, HttpServletRequest request){
+        return rapidAPIService.getStatsChannelFromYoutube(channelName, request);
+    }
+    
+    @GetMapping("/youtube/{videoId}")
+    public ResponseEntity<?> getVideoDetatilsFromYoutube(@PathVariable("videoID")String videoId, HttpServletRequest request){
+        return rapidAPIService.getVideoDetailsFromYoutube(videoId, request);
+    }
+}
---
+++ src/main/java/com/api/service/RapidAPIService.java
@@ -0,0 +1,136 @@
+package com.api.service;
+
+import com.api.dto.ApiResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.DefaultAsyncHttpClient;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RapidAPIService {
+
+    @Value("${spring.x-rapidapi-key}")
+    private String rapidapikey;
+
+    public ResponseEntity<?> getUserStatsFromTiktok(String uniqueId, HttpServletRequest request) {
+        AsyncHttpClient client = new DefaultAsyncHttpClient();
+        final JSONObject[] statsJson = new JSONObject[1];
+        try {
+            client.prepare("GET", "https://tiktok-api23.p.rapidapi.com/api/user/info?uniqueId=" + uniqueId)
+                    .setHeader("x-rapidapi-key", rapidapikey)
+                    .setHeader("x-rapidapi-host", "tiktok-api23.p.rapidapi.com")
+                    .execute()
+                    .toCompletableFuture()
+                    .thenAccept(response -> {
+                        String body = response.getResponseBody();
+                        JSONObject json = new JSONObject(body);
+                        statsJson[0] = json
+                                .getJSONObject("userInfo")
+                                .getJSONObject("statsV2");
+                    }).join();
+
+            client.close();
+        } catch (Exception e) {
+            return ApiResponse.sendError(500, "API service is not available: " + e.getMessage(), request.getRequestURI());
+        }
+        return ApiResponse.sendSuccess(200, "Tiktok response successfully", statsJson[0], request.getRequestURI());
+
+    }
+
+    public ResponseEntity<?> getVideoDetailsFromTiktok(String videoId, HttpServletRequest request) {
+        AsyncHttpClient client = new DefaultAsyncHttpClient();
+        final JSONObject[] statsJson = new JSONObject[1];
+        try {
+            client.prepare("GET", "https://tiktok-api23.p.rapidapi.com/api/post/detail?videoId=" + videoId)
+                    .setHeader("x-rapidapi-key", rapidapikey)
+                    .setHeader("x-rapidapi-host", "tiktok-api23.p.rapidapi.com")
+                    .execute()
+                    .toCompletableFuture()
+                    .thenAccept(response -> {
+                        String body = response.getResponseBody();
+                        JSONObject json = new JSONObject(body);
+                        statsJson[0] = json
+                                .getJSONObject("itemInfo")
+                                .getJSONObject("itemStruct")
+                                .getJSONObject("statsV2");
+                    }).join();
+
+            client.close();
+        } catch (Exception e) {
+            return ApiResponse.sendError(500, "API service is not available: " + e.getMessage(), request.getRequestURI());
+        }
+        return ApiResponse.sendSuccess(200, "Tiktok response successfully", statsJson[0], request.getRequestURI());
+    }
+
+    private String getChannelId(String channelName) throws IOException {
+        final JSONObject[] statsJson = new JSONObject[1];
+        AsyncHttpClient client = new DefaultAsyncHttpClient();
+        client.prepare("GET", "https://youtube-v2.p.rapidapi.com/channel/id?channel_name=" + channelName)
+                .setHeader("x-rapidapi-key", rapidapikey)
+                .setHeader("x-rapidapi-host", "youtube-v2.p.rapidapi.com")
+                .execute()
+                .toCompletableFuture()
+                .thenAccept(response -> {
+                    String body = response.getResponseBody();
+                    JSONObject json = new JSONObject(body);
+                    statsJson[0] = json;
+                })
+                .join();
+
+        client.close();
+
+        return statsJson[0].getString("channel_id");
+    }
+
+    public ResponseEntity<?> getStatsChannelFromYoutube(String channelName, HttpServletRequest request) {
+        final JSONObject[] statsJson = new JSONObject[1];
+        try {
+            String channelId = getChannelId(channelName);
+            AsyncHttpClient client = new DefaultAsyncHttpClient();
+            client.prepare("GET", "https://youtube-v2.p.rapidapi.com/channel/details?channel_id=" + channelId)
+                    .setHeader("x-rapidapi-key", rapidapikey)
+                    .setHeader("x-rapidapi-host", "youtube-v2.p.rapidapi.com")
+                    .execute()
+                    .toCompletableFuture()
+                    .thenAccept(response -> {
+                        String body = response.getResponseBody();
+                        JSONObject json = new JSONObject(body);
+                        statsJson[0] = json;
+                    })
+                    .join();
+
+            client.close();
+        } catch (Exception e) {
+            return ApiResponse.sendError(500, "API service is not available: " + e.getMessage(), request.getRequestURI());
+        }
+        return ApiResponse.sendSuccess(200, "Youtube response successfully", statsJson[0].getString("subscriber_count"), request.getRequestURI());
+    }
+
+    public ResponseEntity<?> getVideoDetailsFromYoutube(String videoId, HttpServletRequest request) {
+        final JSONObject[] statsJson = new JSONObject[1];
+        try {
+            AsyncHttpClient client = new DefaultAsyncHttpClient();
+            client.prepare("GET", "https://youtube-v2.p.rapidapi.com/video/details?video_id=" + videoId)
+                    .setHeader("x-rapidapi-key", rapidapikey)
+                    .setHeader("x-rapidapi-host", "youtube-v2.p.rapidapi.com")
+                    .execute()
+                    .toCompletableFuture()
+                    .thenAccept(response -> {
+                        String body = response.getResponseBody();
+                        JSONObject json = new JSONObject(body);
+                        statsJson[0] = json;
+                    })
+                    .join();
+
+            client.close();
+        } catch (Exception e) {
+            return ApiResponse.sendError(500, "API service is not available: " + e.getMessage(), request.getRequestURI());
+        }
+        return ApiResponse.sendSuccess(200, "Youtube response successfully", statsJson[0], request.getRequestURI());
+    }
+
+}
