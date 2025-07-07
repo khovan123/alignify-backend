0f56b91eb0a83857a0a6848602144c6400f489d0
nhibao08 <lebaonhi0805@gmail.com>
2025-07-07 16:25:56 +0200

    <fix/campaign>: ensure get campaign success by influ

--- src/main/java/com/api/config/MongoConfig.java
+++ src/main/java/com/api/config/MongoConfig.java
@@ -56,8 +56,8 @@ public class MongoConfig {
     // this.create_campaignsCollection(db);
     // this.create_contentPostingsCollection(db);
     // this.create_likesCollection(db);
-    this.create_applicationsCollection(db);
-    this.create_campaignTrackingsCollection(db);
+    // this.create_applicationsCollection(db);
+    // this.create_campaignTrackingsCollection(db);
     // this.create_commentsCollection(db);
     // this.create_chatRoomsCollection(db);
     // this.create_messagesCollection(db);
