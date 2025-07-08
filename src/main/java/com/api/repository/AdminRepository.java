f0dc467750c31f79c367feb7188a6e3651ba02c0
nhibao08 <lebaonhi0805@gmail.com>
2025-07-08 08:27:42 +0200

    <fix/admin>: delete non attribute exist in repository

--- src/main/java/com/api/repository/AdminRepository.java
+++ src/main/java/com/api/repository/AdminRepository.java
@@ -1,14 +1,11 @@
 package com.api.repository;
 
-import com.api.model.Admin;
-import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+
+import com.api.model.Admin;
 
 @Repository
 public interface AdminRepository extends MongoRepository<Admin, String> {
 
-    Optional<Admin> findByEmail(String email);
-
-    boolean existsByEmail(String email);
 }
