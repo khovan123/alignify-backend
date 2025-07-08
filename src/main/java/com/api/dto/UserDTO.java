0e22c3bd2f29de04380c3a2df9900610bb85ddee
Phan Nguyễn Quốc Minh <152273723+khovan123@users.noreply.github.com>
2025-07-09 02:38:58 +0700

    <feat/user-mangemet>: ban, unband, get --users for admin role (#183)
    * <fix/body>: change admin to registerRequest model
    
    * <fix/admin>: delete non attribute exist in repository
    
    * <fix/admin>: small bug
    
    * <feat/security>: allow endpoint register with admin role
    
    * <fix/cors>: allow register with admin role
    
    * <fix/cors>: allow register with admin role
    
    * <chore>: re-build without reset collections
    
    * <fix/admin>: store account in user collections
    
    * <feat/userBans>: add method for return influencer(Ban/Normal)&brand(Ban/Normal)
    
    * <fix/admin>: return user model via userDTO
    
    * <chore>: remove unused import
    
    * <feat/security>: SecurityContext
    
    * <feat/security>: SecurityContext in Send and Connect
    
    * <chore/admin>: create if not exist when login
    
    * <fix/security>:remove pre-handler
    
    * <fix/mongo>: require properties
    
    * <feat/admin>: unband user
    
    ---------
    
    Co-authored-by: nhibao08 <lebaonhi0805@gmail.com>

--- src/main/java/com/api/dto/UserDTO.java
+++ src/main/java/com/api/dto/UserDTO.java
@@ -1,15 +1,26 @@
 package com.api.dto;
 
+import java.time.ZonedDateTime;
+
 public class UserDTO {
 
     private String userId;
     private String name;
     private String avatarUrl;
+    private ZonedDateTime createdAt;
+
+    public UserDTO(String userId, String name, String avatarUrl, ZonedDateTime createdAt) {
+        this.userId = userId;
+        this.name = name;
+        this.avatarUrl = avatarUrl;
+        this.createdAt = createdAt;
+    }
 
     public UserDTO(String userId, String name, String avatarUrl) {
         this.userId = userId;
         this.name = name;
         this.avatarUrl = avatarUrl;
+        this.createdAt = null;
     }
 
     public String getUserId() {
@@ -34,5 +45,13 @@ public class UserDTO {
 
     public void setAvatarUrl(String avatarUrl) {
         this.avatarUrl = avatarUrl;
+    }
+
+    public ZonedDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(ZonedDateTime createdAt) {
+        this.createdAt = createdAt;
     }
 }
