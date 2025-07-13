a0521dcaf80da654a7e48f91c88facd2ad0ffd01
khovan <minhpnq1807@gmail.com>
2025-07-13 16:37:08 +0700

    <fix/fetch-data>: throw runtime exception

--- src/main/java/com/api/service/GeminiService.java
+++ src/main/java/com/api/service/GeminiService.java
@@ -33,129 +33,121 @@ import java.util.Optional;
 @Service
 public class GeminiService {
 
-  @Autowired
-  private ObjectMapper objectMapper;
-  @Autowired
-  private UserRepository userRepository;
-  @Autowired
-  private CampaignRepository campaignRepository;
-  @Value("${gemini.api.key}")
-  private String apiKey;
-  private Client client;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private CampaignRepository campaignRepository;
+    @Value("${gemini.api.key}")
+    private String apiKey;
+    private Client client;
 
-  @PostConstruct
-  public void initGeminiClient() {
-    this.client = Client.builder().apiKey(apiKey).build();
-  }
-
-  @Autowired
-  private InfluencerRepository influencerRepository;
-  @Autowired
-  private CategoryRepository categoryRepository;
-
-  public ResponseEntity<?> getInfluencerRecommendations(String campaignId, CustomUserDetails userDetails,
-      HttpServletRequest request) {
-    String brandId = userDetails.getUserId();
-    Optional<Campaign> campaignOpt = campaignRepository.findByCampaignIdAndBrandId(campaignId, brandId);
-    if (!campaignOpt.isPresent()) {
-      return ApiResponse.sendError(404, "Not found campaign with Id: " + campaignId, request.getRequestURI());
+    @PostConstruct
+    public void initGeminiClient() {
+        this.client = Client.builder().apiKey(apiKey).build();
     }
-    Campaign campaign = campaignOpt.get();
-    try {
-      List<User> users = userRepository.findByRoleIdAndUserIdNotIn(EnvConfig.INFLUENCER_ROLE_ID,
-          campaign.getJoinedInfluencerIds());
-      List<InfluencerProfileResponse> influencerProfileResponses = users.stream().map(user -> {
-        Influencer influencer = influencerRepository.findById(user.getUserId()).get();
-        InfluencerProfileResponse influencerProfileResponse = new InfluencerProfileResponse(user, influencer,
-            categoryRepository);
-        return influencerProfileResponse;
-      }).toList();
-      String prompt = "Based on campaign requirements: " + toJson(campaign)
-          + ", recommend influencers from: " + toJson(influencerProfileResponses)
-          + ". Return a JSON array of influencer profile response in: " + InfluencerProfileResponse.class;
 
-      GenerateContentResponse response = client.models.generateContent(
-          "gemini-1.5-flash",
-          prompt,
-          null);
+    @Autowired
+    private InfluencerRepository influencerRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
-      List<InfluencerProfileResponse> recommendedInfluencers = objectMapper.readValue(
-          response.text(),
-          objectMapper.getTypeFactory().constructCollectionType(List.class, InfluencerProfileResponse.class));
-      Map<String, Object> data = new HashMap<>();
-      data.put("recommendedInfluencers", recommendedInfluencers);
+    public ResponseEntity<?> getInfluencerRecommendations(String campaignId, CustomUserDetails userDetails,
+            HttpServletRequest request) {
+        String brandId = userDetails.getUserId();
+        Optional<Campaign> campaignOpt = campaignRepository.findByCampaignIdAndBrandId(campaignId, brandId);
+        if (!campaignOpt.isPresent()) {
+            return ApiResponse.sendError(404, "Not found campaign with Id: " + campaignId, request.getRequestURI());
+        }
+        Campaign campaign = campaignOpt.get();
+        try {
+            List<User> users = userRepository.findByRoleIdAndUserIdNotIn(EnvConfig.INFLUENCER_ROLE_ID,
+                    campaign.getJoinedInfluencerIds());
+            List<InfluencerProfileResponse> influencerProfileResponses = users.stream().map(user -> {
+                Influencer influencer = influencerRepository.findById(user.getUserId()).get();
+                InfluencerProfileResponse influencerProfileResponse = new InfluencerProfileResponse(user, influencer,
+                        categoryRepository);
+                return influencerProfileResponse;
+            }).toList();
+            String prompt = "Based on campaign requirements: " + toJson(campaign)
+                    + ", recommend influencers from: " + toJson(influencerProfileResponses)
+                    + ". Return a JSON array of influencer profile response in: " + InfluencerProfileResponse.class;
 
-      return ApiResponse.sendSuccess(
-          200,
-          "Influencer recommendations retrieved successfully",
-          data,
-          request.getRequestURI());
-    } catch (JsonProcessingException e) {
-      return ApiResponse.sendError(
-          500,
-          "Failed to retrieve influencer recommendations: " + e.getMessage(),
-          request.getRequestURI());
+            GenerateContentResponse response = client.models.generateContent(
+                    "gemini-1.5-flash",
+                    prompt,
+                    null);
+
+            List<InfluencerProfileResponse> recommendedInfluencers = objectMapper.readValue(
+                    response.text(),
+                    objectMapper.getTypeFactory().constructCollectionType(List.class, InfluencerProfileResponse.class));
+            Map<String, Object> data = new HashMap<>();
+            data.put("recommendedInfluencers", recommendedInfluencers);
+
+            return ApiResponse.sendSuccess(
+                    200,
+                    "Influencer recommendations retrieved successfully",
+                    data,
+                    request.getRequestURI());
+        } catch (JsonProcessingException e) {
+            return ApiResponse.sendError(
+                    500,
+                    "Failed to retrieve influencer recommendations: " + e.getMessage(),
+                    request.getRequestURI());
+        }
     }
-  }
 
-  public ResponseEntity<?> getCampaignRecommendations(CustomUserDetails userDetails, HttpServletRequest request) {
-    String influencerId = userDetails.getUserId();
-    try {
-      User user = userRepository.findById(influencerId)
-          .orElseThrow(() -> new ResourceNotFoundException("User not found with ID: " + influencerId));
-      Influencer influencer = influencerRepository.findById(influencerId)
-          .orElseThrow(() -> new ResourceNotFoundException("Influencer not found with ID: " + influencerId));
-      InfluencerProfileResponse influencerProfileResponse = new InfluencerProfileResponse(user, influencer,
-          categoryRepository);
-      List<Campaign> campaigns = campaignRepository.findAllByStatusOrderByCreatedAtDesc("RECRUITING");
-      List<CampaignResponse> campaignResponses = campaigns.stream().map(campaign -> {
-        User brand = userRepository.findById(campaign.getBrandId())
-            .orElseThrow(() -> new ResourceNotFoundException("Brand not found with ID: " + campaign.getBrandId()));
-        CampaignResponse campaignResponse = new CampaignResponse(brand, campaign, categoryRepository);
-        return campaignResponse;
-      }).toList();
-      String prompt = "Analyze campaign details: " + toJson(campaignResponses)
-          + ". Recommend campaigns for influencer: " + toJson(
-              influencerProfileResponse)
-          + ". Return a JSON array of campaign response in: " + CampaignResponse.class;
+    public ResponseEntity<?> getCampaignRecommendations(CustomUserDetails userDetails, HttpServletRequest request) {
+        String influencerId = userDetails.getUserId();
+        try {
+            User user = userRepository.findById(influencerId)
+                    .orElseThrow(() -> new RuntimeException("User not found with ID: " + influencerId));
+            Influencer influencer = influencerRepository.findById(influencerId)
+                    .orElseThrow(() -> new RuntimeException("Influencer not found with ID: " + influencerId));
+            InfluencerProfileResponse influencerProfileResponse = new InfluencerProfileResponse(user, influencer,
+                    categoryRepository);
+            List<Campaign> campaigns = campaignRepository.findAllByStatusOrderByCreatedAtDesc("RECRUITING");
+            List<CampaignResponse> campaignResponses = campaigns.stream().map(campaign -> {
+                User brand = userRepository.findById(campaign.getBrandId())
+                        .orElseThrow(() -> new RuntimeException("Brand not found with ID: " + campaign.getBrandId()));
+                CampaignResponse campaignResponse = new CampaignResponse(brand, campaign, categoryRepository);
+                return campaignResponse;
+            }).toList();
+            String prompt = "Analyze campaign details: " + toJson(campaignResponses)
+                    + ". Recommend campaigns for influencer: " + toJson(
+                            influencerProfileResponse)
+                    + ". Return a JSON array of campaign response in: " + CampaignResponse.class;
 
-      GenerateContentResponse response = client.models.generateContent(
-          "gemini-1.5-flash",
-          prompt,
-          null);
+            GenerateContentResponse response = client.models.generateContent(
+                    "gemini-1.5-flash",
+                    prompt,
+                    null);
 
-      List<CampaignResponse> recommendedCampaigns = objectMapper.readValue(
-          response.text(),
-          objectMapper.getTypeFactory().constructCollectionType(List.class, CampaignResponse.class));
-      Map<String, Object> data = new HashMap<>();
-      data.put("recommendedCampaigns", recommendedCampaigns);
+            List<CampaignResponse> recommendedCampaigns = objectMapper.readValue(
+                    response.text(),
+                    objectMapper.getTypeFactory().constructCollectionType(List.class, CampaignResponse.class));
+            Map<String, Object> data = new HashMap<>();
+            data.put("recommendedCampaigns", recommendedCampaigns);
 
-      return ApiResponse.sendSuccess(
-          200,
-          "Campaign recommendations retrieved successfully",
-          data,
-          request.getRequestURI());
-    } catch (JsonProcessingException e) {
-      return ApiResponse.sendError(
-          500,
-          "Failed to retrieve campaign recommendations: " + e.getMessage(),
-          request.getRequestURI());
+            return ApiResponse.sendSuccess(
+                    200,
+                    "Campaign recommendations retrieved successfully",
+                    data,
+                    request.getRequestURI());
+        } catch (JsonProcessingException e) {
+            return ApiResponse.sendError(
+                    500,
+                    "Failed to retrieve campaign recommendations: " + e.getMessage(),
+                    request.getRequestURI());
+        }
     }
-  }
 
-  private String toJson(Object obj) {
-    try {
-      return objectMapper.writeValueAsString(obj);
-    } catch (JsonProcessingException e) {
-      return "{}";
+    private String toJson(Object obj) {
+        try {
+            return objectMapper.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            return "{}";
+        }
     }
-  }
-
-  private List<String> parseRecommendations(String response) {
-    try {
-      return objectMapper.readValue(response, List.class);
-    } catch (JsonProcessingException e) {
-      return new ArrayList<>();
-    }
-  }
 }
